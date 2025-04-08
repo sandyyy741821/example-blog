449bffc548a3e4c3e5d89b177666008ce41032fc
sandyyy741821 <santhoshsandyyy741821@gmail.com>
2025-04-08 10:19:58 +0530

    this is blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://astro.build/config
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  vite: {
+    server: {
+      host: true,
+      port: 4321,
+      strictPort: true,
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, 'example.com+2-key.pem')),
+        cert: fs.readFileSync(path.resolve(__dirname, 'example.com+2-cert.pem')),
+      },
+      allowedHosts: ['example.com','sub.example.com'],
+    }
+  }
+  
 });
