6f3ba0ec90b792f2d14a12970736f858b57e4af3
sandyyy741821 <santhoshsandyyy741821@gmail.com>
2025-04-08 17:25:11 +0530

    added netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,19 +8,25 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://mainfrontendnet.netlify.app',
   integrations: [mdx(), sitemap()],
   vite: {
     server: {
       host: true,
       port: 4321,
       strictPort: true,
-      // https: {
-      //   key: fs.readFileSync(path.resolve(__dirname, 'example.com+2-key.pem')),
-      //   cert: fs.readFileSync(path.resolve(__dirname, 'example.com+2-cert.pem')),
-      // },
-      allowedHosts: ['example.com','sub.example.com'],
     }
-  }
-  
+  },
+  build: {
+    outDir: './dist',
+  },
+  renderers: [
+    mdx(),
+  ],
+  pages: [
+    {
+      path: '/blog/*', 
+      component: './src/pages/blog/*.astro', 
+    },
+  ],
 });
